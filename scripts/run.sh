b93f91ec22396eb247f3ecad0be6ed0c5004a4c9
Carter Green <cartergr@usc.edu>
2017-11-09 17:03:21 -0800

    v0.2.0 * Fix unit conversions * Standardize button colors * Update About page * Add logo to navbar * Responsive tables * Update run script to automatically open browser * Better table highlighting

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,16 @@ dir="$(dirname $0)"
 parentdir="$(dirname "$dir")"
 cp -a $parentdir/data/wine.db "$parentdir/data/wine_$(date +"%y%m%d_%H%M%S").db"
 source activate vinoteca
+# Run browser
+echo "Attempting to open browser..."
+cmd=false
+case `uname` in
+    Darwin) cmd=open;;
+    Linux)  cmd=xdg-open;;
+    *) echo "Don't know how to open URLs on "`uname`;;
+esac
+if [ "$cmd" != "false" ] ; then
+    (sleep 4;$cmd http://localhost:8000)&
+fi
+# Run django server
 python manage.py runserver
