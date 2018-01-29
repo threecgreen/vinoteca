70cd76bd8304c144dce6988225fa005ee9c6ebab
Carter Green <cartergr@usc.edu>
2018-01-28 20:01:29 -0800

    Better error handling

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
+set -e
 source activate vinoteca
 dir="$(dirname $0)"
 parentdir="$(dirname "$dir")"
-pytest --cov --cov-config="$parentdir/tests/.coveragerc"
+pytest --cov --cov-config="$parentdir/tests/.coveragerc" "$parentdir"
