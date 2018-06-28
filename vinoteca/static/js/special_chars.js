198e795611bb1363cf28f980556677f7723ca30e
Carter Green <cartergr@usc.edu>
2018-06-28 18:06:42 -0500

    Rename some variables

--- vinoteca/static/js/special_chars.js
+++ vinoteca/static/js/special_chars.js
@@ -1,30 +1,31 @@
-const $spec_char_btns = $(".spec-chars");
-const $shift_btn = $("#shift");
-let $text_inputs = $("[type=text]");
+const spec_char_btns = $(".spec-chars");
+const shift_btn = $("#shift");
+let text_inputs = $("[type=text]");
 /* Keep track of last active text element */
-let $last_input = null;
+let last_input = null;
+
 $(function() {
     /* Add special character to last input */
-    $spec_char_btns.on("click", function () {
-        $($last_input).val($($last_input).val() + $(this).text());
+    $(spec_char_btns).on("click", function () {
+        last_input = last_input + $(this).text();
     });
     /* Change special character buttons between upper and lowercase */
-    $shift_btn.on("click", function () {
-        if ($shift_btn.text() === "↑") {
-            $spec_char_btns.each(function () {
+    $(shift_btn).on("click", function () {
+        if ($(shift_btn).text() === "↑") {
+            $(spec_char_btns).each(function () {
                 $(this).text($(this).text().toUpperCase());
             });
-            $shift_btn.text("↓");
+            $(shift_btn).text("↓");
         }
         else {
-            $spec_char_btns.each(function () {
+            $(spec_char_btns).each(function () {
                 $(this).text($(this).text().toLowerCase());
             });
-            $shift_btn.text("↑");
+            $(shift_btn).text("↑");
         }
     });
-    
-    $text_inputs.on("focusout", function () {
-        $last_input = "#" + this.id;
+    /* Update last active input field each time the focus changes */
+    $(text_inputs).on("focusout", function () {
+        last_input = "#" + this.id;
     });
 });
