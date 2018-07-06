be538f355b4e794ce099774b7924ee8323cf4f2f
Carter Green <cartergr@usc.edu>
2018-07-06 00:04:58 -0500

    JSON response tests

--- tests/test_view.py
+++ tests/test_view.py
@@ -3,7 +3,8 @@ from django.conf import settings
 from pathlib import Path
 import pytest
 
-from vinoteca.models import Producers, Purchases, Wines
+from vinoteca.models import Grapes, Producers, Purchases, Wines, WineGrapes
+from view.views import *
 
 
 @pytest.fixture
@@ -29,6 +30,7 @@ def upload_file():
     "/wines/800/edit/",
     "/wines/752/edit/1/",
     "/producers/75/",
+    "/producers/75/edit/",
     "/regions/3/",
     "/wines/table/",
     "/inventory/"
@@ -45,6 +47,7 @@ def test_pages(client, url):
     ("/wines/800/edit/", "Edit Wine"),
     ("/wines/752/edit/1/", "Edit Purchase"),
     ("/producers/75/", "Producer Profile"),
+    ("/producers/75/edit/", "Edit Producer"),
     ("/regions/3/", "Country Profile"),
     ("/wines/table/", "Wine Table"),
     ("/inventory/", "Inventory")
@@ -55,17 +58,54 @@ def test_urls(url, view_name):
     assert resolver.view_name == view_name
 
 
-@pytest.mark.parametrize("attr,val", [
-    ("description", "Test"),
-    ("producer", "A new producer"),
-    ("color", "white"),
-    ("viti_area", "Mendocino County")
-])
 @pytest.mark.django_db
-def test_edit_wine(client, attr, val):
+def test_colors(client):
+    response = client.get("/colors/all/")
+    assert len(Colors.objects.all()) == len(response.json().keys())
+
+
+@pytest.mark.django_db
+def test_regions(client):
+    response = client.get("/regions/all/")
+    assert len(Countries.objects.all()) == len(response.json().keys())
+    # Check for flags
+    assert ".svg" in response.json()["California"]
+    assert ".svg" in response.json()["France"]
+
+
+@pytest.mark.django_db
+def test_get_producers(client):
+    response = client.get("/producers/all/")
+    assert len(Producers.objects.all()) == len(response.json().keys())
+
+
+@pytest.mark.django_db
+def test_get_stores(client):
+    response = client.get("/stores/all/")
+    assert len(Stores.objects.all()) == len(response.json().keys())
+
+
+@pytest.mark.django_db
+def test_get_grapes(client):
+    response = client.get("/grapes/all/")
+    assert len(Grapes.objects.all()) == len(response.json().keys())
+
+
+@pytest.mark.django_db
+def test_get_wine_types(client):
+    response = client.get("/wine-types/all/")
+    assert len(WineTypes.objects.all()) == len(response.json().keys())
+
+
+@pytest.mark.django_db
+def test_get_viti_areas(client):
+    response = client.get("/viti-areas/all/")
+    assert len(VitiAreas.objects.all()) == len(response.json().keys())
+
+
+@pytest.fixture
+def wine_and_post_data():
     wine = Wines.objects.get(id=800)
-    init_val = wine.__getattribute__(attr)
-    assert init_val != val
     post_data = {
         "producer": wine.producer.name,
         "country": wine.producer.country.name,
@@ -77,6 +117,19 @@ def test_edit_wine(client, attr, val):
         "wine-type": wine.wine_type.type_name,
         "viti-area": wine.viti_area.name if wine.viti_area else None,
     }
+    return wine, post_data
+
+@pytest.mark.parametrize("attr,val", [
+    ("description", "Test"),
+    ("producer", "A new producer"),
+    ("color", "white"),
+    ("viti_area", "Mendocino County")
+])
+@pytest.mark.django_db
+def test_edit_wine(client, wine_and_post_data, attr, val):
+    wine, post_data = wine_and_post_data
+    init_val = wine.__getattribute__(attr)
+    assert init_val != val
     post_data[attr.replace("_", "-")] = val
     response = client.post("/wines/800/edit/", post_data, follow=True)
     assert response.status_code == 200
@@ -87,6 +140,28 @@ def test_edit_wine(client, attr, val):
         assert Wines.objects.get(id=800).__getattribute__(attr).name == val
     else:
         assert Wines.objects.get(id=800).__getattribute__(attr) == val
+
+
+@pytest.mark.django_db
+def test_edit_wine_with_grapes(client, wine_and_post_data):
+    wine, post_data = wine_and_post_data
+    wine_grapes = WineGrapes.objects.filter(wine=wine)
+    assert len(wine_grapes) == 0
+    # Add grape data
+    post_data["grape-1"] = "Malbec"
+    post_data["grape-1-pct"] = 50
+    post_data["grape-2"] = "Syrah"
+    post_data["grape-2-pct"] = 50
+    # Post
+    response = client.post("/wines/800/edit/", post_data, follow=True)
+    assert response.status_code == 200
+    assert ("/wines/800/", 302) in response.redirect_chain
+    wine_grapes = WineGrapes.objects.filter(wine=wine)
+    grapes = [wine_grape.grape for wine_grape in wine_grapes]
+    malbec = Grapes.objects.get(name="Malbec")
+    syrah = Grapes.objects.get(name="Syrah")
+    assert malbec in grapes
+    assert syrah in grapes
 
 
 @pytest.mark.django_db
