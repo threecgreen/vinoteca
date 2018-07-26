91b77fd5a7de3501c062e5379b127e20e1a694dd
Carter Green <cartergr@usc.edu>
2018-07-26 14:36:03 -0500

    Increase readability

--- vinoteca/urls.py
+++ vinoteca/urls.py
@@ -5,18 +5,37 @@ from django.views.static import serve
 from vinoteca.views import simple_page, home
 from dashboards.views import dashboards
 from new_purchase.views import *
-from graph.views import graph
 from view.views import *
 
+wine_patterns = [
+    path("<int:wine_id>/", wine_profile, name="Wine Profile"),
+    path("<int:wine_id>/new-purchase/", prev_purchase, name="New Purchase Wine"),
+    path("<int:wine_id>/edit/", edit_wine, name="Edit Wine"),
+    path("<int:wine_id>/edit/delete/confirmed/", delete_wine, name="Delete Wine"),
+    path("<int:wine_id>/edit/<int:purchase_id>/", edit_purchase, name="Edit Purchase"),
+    path("<int:wine_id>/edit/<int:purchase_id>/delete/", delete_purchase,
+         name="Delete Purchase"),
+    path("<int:wine_id>/change/<slug:sign>/", change_inventory, name="Change Inventory"),
+    path("<int:wine_id>/change/<slug:sign>/inventory/", change_inventory,
+         name="Change Inventory from Inventory", kwargs={"return_to_inventory": True}),
+]
 
 urlpatterns = [
+    # vinoteca app patterns
     path("", home, name="Home"),
     path("home/", home, name="Home"),
     path("about/", simple_page, name="About", kwargs={"page_name": "about"}),
     path("help/", simple_page, name="Help", kwargs={"page_name": "help"}),
     path("changelog/", simple_page, name="Changelog", kwargs={"page_name": "changelog"}),
+
+    # wine patterns
+    path("wines/", include(wine_patterns)),
+
+    # new purchase patterns
     path("new/first-time/", first_new_purchase, name="New Purchase First"),
     path("new/prev-purchased/", prev_new_purchase_search, name="New Purchase Search"),
+
+    # JSON patterns
     path("producers/region/", get_producer_region, name="Get Producer Region JSON"),
     path("regions/viti-areas/", get_region_viti_areas, name="Get Region Viti Areas JSON"),
     path("colors/all/", get_colors, name="Get Colors JSON"),
@@ -27,27 +46,22 @@ urlpatterns = [
     path("wine-types/all/", get_wine_types, name="Get WineTypes JSON"),
     path("viti-areas/all/", get_viti_areas, name="Get VitiAreas JSON"),
     path("new/search-wines/", search_wines, name="Search Wines JSON"),
-    path("wines/<int:wine_id>/", wine_profile, name="Wine Profile"),
-    path("wines/<int:wine_id>/new-purchase/", prev_purchase, name="New Purchase Wine"),
-    path("wines/<int:wine_id>/edit/", edit_wine, name="Edit Wine"),
-    path("wines/<int:wine_id>/edit/delete/confirmed/", delete_wine, name="Delete Wine"),
-    path("wines/<int:wine_id>/edit/<int:purchase_id>/", edit_purchase, name="Edit Purchase"),
-    path("wines/<int:wine_id>/edit/<int:purchase_id>/delete/", delete_purchase,
-         name="Delete Purchase"),
-    path("wines/<int:wine_id>/change/<slug:sign>/", change_inventory, name="Change Inventory"),
+
+    # Other profiles
     path("producers/<int:producer_id>/", producer_profile, name="Producer Profile"),
     path("producers/<int:producer_id>/edit/", edit_producer, name="Edit Producer"),
     path("regions/<int:region_id>/", region_profile, name="Region Profile"),
+    path("wine-types/<int:wine_type_id>/", wine_type_profile, name="Wine Type Profile"),
+
+    # Other pages
     path("wines/table/", wine_table, name="Wine Table"),
     path("dashboards/", dashboards, name="Dashboards"),
     path("inventory/", inventory, name="Inventory"),
-    path("wines/<int:wine_id>/change/<slug:sign>/inventory/", change_inventory,
-         name="Change Inventory from Inventory",
-         kwargs={"return_to_inventory": True}),
-    path("wine-types/<int:wine_type_id>/", wine_type_profile, name="Wine Type Profile"),
-    path("graph/test/", graph, name="Wine Graph"),
+
+    # Graph URLs
     path("graph/", include("graph.urls")),
 
+    # Internal URLs
     re_path("^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     re_path("^js_error_hook/", include("django_js_error_hook.urls")),
 ]
