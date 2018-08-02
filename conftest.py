e25a447b68f6dd9c2c63af8e456447c7fbeeb519
Carter Green <cartergr@usc.edu>
2018-08-02 10:48:44 -0500

    Start test fixtures

--- conftest.py
+++ conftest.py
@@ -3,23 +3,22 @@ Main (along with pytest.ini) pytest configuration file for django testing.
 """
 import pytest
 from django.test import Client
+from django.core.management import call_command
 
 
-@pytest.fixture(scope="session")
-def django_db_setup():
-    """
-    Avoid creating/setting up the test database.
-    """
-    pass
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'tests/test_data_fixtures.json')
 
 
-@pytest.fixture
-def db_access_without_rollback_and_truncate(request, django_db_setup, django_db_blocker):
-    """
-    Second part of avoiding using a test database.
-    """
-    django_db_blocker.unblock()
-    request.addfinalizer(django_db_blocker.restore)
+# @pytest.fixture
+# def db_access_without_rollback_and_truncate(request, django_db_setup, django_db_blocker):
+#     """
+#     Second part of avoiding using a test database.
+#     """
+#     django_db_blocker.unblock()
+#     request.addfinalizer(django_db_blocker.restore)
 
 
 @pytest.fixture
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ source "$(dirname $0)/utils.sh"
 
 find_python_env
 "$py_env/pytest" --cov --cov-config="$root_dir/tests/.coveragerc" "$root_dir" \
-    --cov-report term-missing
+    --cov-report term-missing --create-db
