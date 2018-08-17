116d0bbb2cb1d3c62f0547593cb190416a706f97
Carter Green <cartergr@usc.edu>
2018-08-17 18:39:40 -0500

    Minor documentation

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-# Run linters
+# Runs code linters. If no arguments are specified, all linters are run. To
+# run a specific linter, pass the name of the language. For example:
+# $ ./lint.sh python
+# will pylint, the Python linter.
 source "$(dirname $0)/utils.sh"
 find_python_env
 
-pylint() {
-    # Runs Python linter called pylint
+pylint()
+# Runs Python linter called pylint
+{
     info_text "Running Python linter."
     echo $py_env
     "$py_env/pylint" "$root_dir/dashboards" "$root_dir/new_purchase" "$root_dir/rest" \
@@ -12,8 +16,9 @@ pylint() {
         --rcfile="$root_dir/.pylintrc" --output-format=colorized
 }
 
-typescript() {
-    # Runs typescript linter called tslint
+typescript()
+# Runs typescript linter called tslint
+{
     info_text "Running Typescript linter."
     tslint -c "$root_dir/tslint.json" "$root_dir/vinoteca/static/ts/*.ts" \
             "$root_dir/rest/static/ts/*.ts" --exclude "$root_dir/node_modules" \
