489307e695b47e55fd1ab420c4748e87228d3d6b
Carter Green <cartergr@usc.edu>
2018-08-17 18:36:54 -0500

    Fix errors for linter configs

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,18 +1,40 @@
 #!/usr/bin/env bash
 # Run linters
 source "$(dirname $0)/utils.sh"
+find_python_env
 
-if [ $(command -v pylint) ]; then
+pylint() {
+    # Runs Python linter called pylint
     info_text "Running Python linter."
-    pylint "$root_dir/dashboards" "$root_dir/new_purchase" "$root_dir/rest" \
+    echo $py_env
+    "$py_env/pylint" "$root_dir/dashboards" "$root_dir/new_purchase" "$root_dir/rest" \
         "$root_dir/view" "$root_dir/vinoteca" \
         --rcfile="$root_dir/.pylintrc" --output-format=colorized
-else
-    info_text "Pylint not found; skipping..."
-fi
-if [ $(command -v tslint) ]; then
+}
+
+typescript() {
+    # Runs typescript linter called tslint
     info_text "Running Typescript linter."
-    tslint -c "$root_dir/tslint.json" "$root_dir/**/*.ts" --exclude "$root_dir/node_modules"
-else
-    info_text "TSlint not found; skipping..."
-fi
+    tslint -c "$root_dir/tslint.json" "$root_dir/vinoteca/static/ts/*.ts" \
+            "$root_dir/rest/static/ts/*.ts" --exclude "$root_dir/node_modules" \
+            --format codeFrame
+}
+
+case $# in
+    0)
+        pylint
+        typescript
+        ;;
+    1)
+        if [ $1 = "python" ]; then
+            pylint
+        elif [ $1 = "typescript" ]; then
+            typescript
+        else
+            error_exit "Unknown linter type."
+        fi
+        ;;
+    *)
+        error_exit "Invalid number of arguments."
+        ;;
+esac
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Run Python tests
 source "$(dirname $0)/utils.sh"
-
 find_python_env
+
 "$py_env/pytest" --cov --cov-config="$root_dir/tests/.coveragerc" "$root_dir" \
     --cov-report term-missing --create-db
