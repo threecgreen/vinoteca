d28fdb5bd2ad196fb8c98e960c9dc54993d3c60d
Carter Green <cartergr@usc.edu>
2018-08-20 22:26:46 -0500

    Fix tags

--- vinoteca/templatetags/vinoteca.py
+++ vinoteca/templatetags/vinoteca.py
@@ -3,22 +3,23 @@ django template tags from vinoteca ones.
 
 Loaded with:
     {% load vinoteca %} """
+# pylint: disable=invalid-name
 from django import template
 
 import vinoteca.utils
 
 
-REGISTER = template.Library()
+register = template.Library()
 
 
-@REGISTER.filter(name="inttodate")
+@register.filter(name="inttodate")
 def int_to_date(value):
     """Implement a Django template version of int_to_date to move display of data logic from views
     to templates."""
     return vinoteca.utils.int_to_date(value)
 
 
-@REGISTER.filter(name="defndash", safe=True)
+@register.filter(name="defndash", safe=True)
 def def_ndash(value):
     """Equivalent of |default_if_none:&ndash;"""
     return value if value else "–"
