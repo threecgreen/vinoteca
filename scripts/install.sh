48cc25e9e2bd1e45ab767f8b1f483e91731c1421
Carter Green <cartergr@usc.edu>
2018-10-21 01:04:16 -0500

    Update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,32 +1,44 @@
 #!/usr/bin/env bash
 set -e
+
+source "$(dirname $0)/utils.sh"
+
 # Install script for Debian-based systems
 # Get admin privileges before running
 sudo echo
 # Assumes nothing is installed
-echo "Updating system..."
+info_text "Updating system..."
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install git execstack nodejs
 
 # Download Python
-echo "Downloading Miniconda Python distribution..."
+info_text "Downloading Miniconda Python distribution..."
 wget repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 # Install Python
-echo "Installing..."
+info_text "Installing..."
 bash Miniconda3-latest-Linux-x86_64.sh -p $HOME/miniconda -b
 # add miniconda to PATH
-echo -e "export PATH=\"\$HOME/miniconda/bin:\$PATH\"" >> $HOME/.bashrc
+info_text -e "export PATH=\"\$HOME/miniconda/bin:\$PATH\"" >> $HOME/.bashrc
 bin=$HOME/miniconda/bin
 
 # Create environment
-echo "Creating a new python virtual environment called 'vinoteca'..."
+info_text "Creating a new python virtual environment called 'vinoteca'..."
 execstack -c $HOME/miniconda/lib/libcrypto.so.1.0.0
 $bin/conda create -n vinoteca -y python=3.6
 source $bin/activate vinoteca
-echo "Installing vinoteca dependencies..."
+info_text "Installing vinoteca dependencies..."
 $bin/pip install -r requirements.txt
+
+# Javascript
+info_text "Installing NPM dependencies..."
+check_for_node
+cd vinoteca
+npm install --save-dev --no-optional
+info_text "Building webpack bundles..."
+npm run-script build
+cd "$root_dir"
 
 # Finishing up
 echo
