f02b5463cf22d09ff78c2bf78edd4463293c09af
Carter Green <cartergr@usc.edu>
2018-10-30 23:34:24 -0500

    Query change to attempt NV fix

--- dashboards/views.py
+++ dashboards/views.py
@@ -219,6 +219,7 @@ def inventory(request):
                     , max(p2.date) as last_purchase_date
                 FROM wines w2
                     INNER JOIN purchases p2 ON w2.id = p2.wine_id
+                WHERE p2.vintage IS NOT NULL
                 GROUP BY w2.id
             ) AS sub ON sub.id = w.id
             LEFT JOIN purchases p3 ON w.id = p3.wine_id AND p3.date = sub.last_purchase_date
