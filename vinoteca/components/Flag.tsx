6d75d0e0dd204ae468750d442e9bdd08c4b6b71a
Carter Green <cartergr@usc.edu>
2019-12-09 21:57:24 -0600

    Clean up flag

--- vinoteca/components/Flag.tsx
+++ vinoteca/components/Flag.tsx
@@ -1,26 +1,15 @@
 import * as React from "react";
 
-interface IFlagProps {
+interface IProps {
     region: string;
 }
 
-interface IFlagState {
-    exists: boolean;
-}
-
-export class Flag extends React.Component<IFlagProps, IFlagState> {
-    constructor(props: IFlagProps) {
-        super(props);
-        this.state = { exists: false };
-    }
-
-    public render() {
-        return (
-            <img src={ `/static/img/flags/${ this.props.region }.svg` }
-                alt={ `Flag of ${this.props.region}` }
-                className="circle z-depth-2 img-responsive region-flag"
-                onError={ undefined }
-            />
-        );
-    }
-}
+export const Flag: React.FunctionComponent<IProps> = (props) => {
+    return (
+        <img src={ `/static/img/flags/${props.region}.svg` }
+            alt={ `Flag of ${props.region}` }
+            className="circle z-depth-2 img-responsive region-flag"
+        />
+    );
+};
+Flag.displayName = "Flag";
