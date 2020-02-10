f68b291f7bda30c51d337516316b3fdec8d92f80
Carter Green <cartergr@usc.edu>
2020-02-09 18:00:25 -0600

    Fix ignores

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ source "$(dirname $0)/utils.sh"
 
 rust_build release
 js_install_and_build
-mkdir out
-cp ./target/release/server out/vinoteca
-strip ./out/vinoteca
-cp -r ./web/static out
+mkdir vinoteca
+cp ./target/release/server vinoteca/vinoteca
+strip ./vinoteca/vinoteca
+cp -r ./web/static vinoteca
 
-tar --create --gzip --file vinoteca.tar.gz --directory=out
+tar --create --gzip --file vinoteca.tar.gz vinoteca
