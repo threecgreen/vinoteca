9acc6aa5bc8ea67579dc76df407db0a978cd30cb
Carter Green <cartergr@usc.edu>
2020-02-18 21:09:31 -0600

    Remove update sh

--- scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Downloads and extracts latest release
-
-curl --silent "https://api.github.com/repos/threecgreen/vinoteca/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/' |
-    xargs -I {} curl -sOL "https://github.com/threecgreen/vinoteca/archive/"\{\}'.tar.gz'
-tar -xzf *.tar.gz
-cd vinoteca
