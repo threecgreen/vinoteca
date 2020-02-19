596b467a41b880e5ebca43405b269bd0edf7f012
Carter Green <cartergr@usc.edu>
2020-02-18 21:08:17 -0600

    Fix nullable error

--- src/wines/inventory.sql
+++ src/wines/inventory.sql
@@ -13,12 +13,12 @@ SELECT
     , w.inventory AS inventory
     , p3.price AS last_purchase_price
 FROM wines w
-    LEFT JOIN producers p ON w.producer_id = p.id
-    LEFT JOIN regions r ON p.region_id = r.id
-    LEFT JOIN colors c ON w.color_id = c.id
-    LEFT JOIN wine_types wt ON w.wine_type_id = wt.id
-    LEFT JOIN purchases pu ON w.id = pu.wine_id
-    LEFT JOIN (
+    INNER JOIN producers p ON w.producer_id = p.id
+    INNER JOIN regions r ON p.region_id = r.id
+    INNER JOIN colors c ON w.color_id = c.id
+    INNER JOIN wine_types wt ON w.wine_type_id = wt.id
+    INNER JOIN purchases pu ON w.id = pu.wine_id
+    INNER JOIN (
         SELECT
             w2.id
             , max(p2.date) as last_purchase_date
@@ -27,7 +27,7 @@ FROM wines w
         WHERE p2.vintage IS NOT NULL
         GROUP BY w2.id
     ) AS sub ON sub.id = w.id
-    LEFT JOIN purchases p3 ON w.id = p3.wine_id
+    INNER JOIN purchases p3 ON w.id = p3.wine_id
         AND (p3.date = sub.last_purchase_date
             OR sub.last_purchase_date IS NULL)
 WHERE w.inventory > 0
