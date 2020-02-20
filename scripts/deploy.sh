22caa53246986cceca8ba4a71541713c42e27988
Carter Green <cartergr@usc.edu>
2020-02-19 20:41:07 -0600

    Build deb

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,5 @@ source "$(dirname $0)/utils.sh"
 
 rust_build release
 js_install_and_build
-mkdir vinoteca
-cp ./target/release/server vinoteca/vinoteca
-strip ./vinoteca/vinoteca
-cp -r ./web/static vinoteca
-
-tar --create --gzip --file vinoteca.tar.gz vinoteca
+cargo install cargo-deb
+cargo deb
