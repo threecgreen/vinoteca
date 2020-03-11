ce62ff6846bb86b354e6ebafbe303d3828ea6ff7
Carter Green <cartergr@usc.edu>
2020-03-10 23:25:18 -0500

    Id issue with images

--- src/wines/image.rs
+++ src/wines/image.rs
@@ -7,6 +7,7 @@ pub fn handle_image(wine: &Wine, raw_img: Vec<u8>) -> Result<(), VinotecaError> 
     let img = image::io::Reader::new(Cursor::new(raw_img))
         .with_guessed_format()?
         .decode()?;
+    // FIXME: read from config for this path
     img.save(format!("media/{}.png", wine.id))?;
     Ok(())
 }
