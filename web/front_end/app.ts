58f843f86ba72c38c10a4c220539a51b8ce9be52
Carter Green <cartergr@usc.edu>
2020-03-22 11:32:33 -0500

    window-level error handling

--- web/front_end/app.ts
+++ web/front_end/app.ts
@@ -1,11 +1,12 @@
 import { createElement } from "react";
 import { render } from "react-dom";
-import { onLoad } from "../lib/utils";
+import { onLoad, onError } from "../lib/utils";
 import { Footer } from "./Footer";
 import { Top } from "./Navbar";
 import { Router } from "./Router";
 
 onLoad(() => {
+    window.onerror = onError;
     render(createElement(Top), document.getElementById("navbar-container"));
     render(createElement(Router), document.getElementById("app-container"));
     render(createElement(Footer), document.getElementById("footer-container"));
--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -1,5 +1,6 @@
 /** Basic type that corresponds to the response JSON of many asynchronous requests. */
 import { IRestModel } from "./RestTypes";
+import Logger from "./Logger";
 
 /**
  * Key-value store where the key must be a string, but the value is of any type
@@ -163,4 +164,10 @@ export function getNameAndType(name: string | null, wineType: string): string {
 
 export function onLoad(fun: () => void) {
     document.addEventListener("DOMContentLoaded", fun);
+}
+
+export function onError(event: Event | string, source?: string, line?: number, col?: number, error?: Error) {
+    new Logger("window", false, false).logCritical(
+        `A top-level error occured at line ${line}:${col} of ${source}: ${error?.message},`
+        + ` event: ${event.toString()}`);
 }
