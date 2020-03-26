2cc7d9f67cc2381a73124ae5f584b08672518751
Carter Green <cartergr@usc.edu>
2020-03-26 04:45:01 -0500

    Create wines

---
+++ migrations/2020-03-26-043006_create_wines/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE wines (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    notes TEXT,
+    why TEXT,
+    inventory INTEGER NOT NULL CHECK (inventory > 0),
+    rating INTEGER CHECK (rating BETWEEN 0 AND 10),
+    color_id INTEGER NOT NULL REFERENCES colors(id) ON DELETE RESTRICT,
+    wine_type_id INTEGER NOT NULL REFERENCES wine_types(id) ON DELETE RESTRICT,
+    producer_id INTEGER NOT NULL REFERENCES producers(id) ON DELETE RESTRICT,
+    viti_area_id INTEGER REFERENCES viti_areas(id) ON DELETE RESTRICT,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
