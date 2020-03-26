8311023f80d5ea3085065d9f9333855a8dcce7b8
Carter Green <cartergr@usc.edu>
2020-03-26 06:35:20 -0500

    Add wine image

--- migrations/2020-03-26-043006_create_wines/up.sql
+++ migrations/2020-03-26-043006_create_wines/up.sql
@@ -6,6 +6,7 @@ CREATE TABLE wines (
     why TEXT,
     inventory INTEGER NOT NULL CHECK (inventory > 0),
     rating INTEGER CHECK (rating BETWEEN 0 AND 10),
+    image TEXT,
     color_id INTEGER NOT NULL REFERENCES colors(id) ON DELETE RESTRICT,
     wine_type_id INTEGER NOT NULL REFERENCES wine_types(id) ON DELETE RESTRICT,
     producer_id INTEGER NOT NULL REFERENCES producers(id) ON DELETE RESTRICT,
