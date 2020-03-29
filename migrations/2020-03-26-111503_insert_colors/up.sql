c2c54753a20d841fa7b1ff7aa9f4fd98374269fd
Carter Green <cartergr@usc.edu>
2020-03-29 00:04:22 -0500

    Fix colors ids

--- migrations/2020-03-26-111503_insert_colors/up.sql
+++ migrations/2020-03-26-111503_insert_colors/up.sql
@@ -1,15 +1,15 @@
 -- Insert colors
-INSERT INTO colors (name)
-VALUES ('red') ON CONFLICT DO NOTHING;
-INSERT INTO colors (name)
-VALUES ('white') ON CONFLICT DO NOTHING;
-INSERT INTO colors (name)
-VALUES ('rosé') ON CONFLICT DO NOTHING;
-INSERT INTO colors (name)
-VALUES ('dessert') ON CONFLICT DO NOTHING;
-INSERT INTO colors (name)
-VALUES ('sparkling') ON CONFLICT DO NOTHING;
-INSERT INTO colors (name)
-VALUES ('port') ON CONFLICT DO NOTHING;
-INSERT INTO colors (name)
-VALUES ('cider') ON CONFLICT DO NOTHING;
+INSERT INTO colors (id, name)
+VALUES (1, 'red') ON CONFLICT DO NOTHING;
+INSERT INTO colors (id, name)
+VALUES (2, 'white') ON CONFLICT DO NOTHING;
+INSERT INTO colors (id, name)
+VALUES (3, 'rosé') ON CONFLICT DO NOTHING;
+INSERT INTO colors (id, name)
+VALUES (4, 'dessert') ON CONFLICT DO NOTHING;
+INSERT INTO colors (id, name)
+VALUES (5, 'sparkling') ON CONFLICT DO NOTHING;
+INSERT INTO colors (id, name)
+VALUES (6, 'port') ON CONFLICT DO NOTHING;
+INSERT INTO colors (id, name)
+VALUES (7, 'cider') ON CONFLICT DO NOTHING;
