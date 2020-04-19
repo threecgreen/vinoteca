8b3c0de4ea9c479053eb06dddc9aba2ca57cae21
Carter Green <cartergr@usc.edu>
2020-04-19 18:17:48 -0500

    Convert wines to FC

--- web/front_end/wines/WinesApp.tsx
+++ web/front_end/wines/WinesApp.tsx
@@ -8,7 +8,9 @@ import FilterExpr from "../../lib/FilterExpr";
 import Logger from "../../lib/Logger";
 import { IWine } from "../../lib/Rest";
 import { getWines } from "../../lib/RestApi";
-import { setTitle } from "../../lib/widgets";
+import { useTitle } from "../../lib/widgets";
+
+const LOCAL_STORAGE_KEY = "WinesAppPredicates";
 
 interface IState {
     wines: IWine[];
@@ -19,164 +21,161 @@ interface IState {
     winesPerPage: number;
 }
 
-export class WinesApp extends React.Component<{}, IState> {
-    private readonly logger: Logger;
-    private static localStorageKey: string = "WinesAppPredicates";
+type Action =
+    | { type: "setWines", wines: IWine[] }
+    | { type: "setFilter", column: WinesTableColumn, text: string }
+    | { type: "resetFilters" }
+    | { type: "setCurrentPage", currentPage: number };
 
-    public constructor(props: {}) {
-        super(props);
-
-        this.logger = new Logger("WinesApp");
-
-        const filterTexts = this.deserializeFilters(window.localStorage.getItem(WinesApp.localStorageKey) ?? "")
-        this.state = {
-            wines: [],
-            filterTexts: filterTexts,
-            predicates: this.parseAllFilters(filterTexts),
-            hasLoaded: false,
-            currentPage: 1,
-            winesPerPage: 50,
-        };
+const deserializeFilters = (json: string): Map<WinesTableColumn, string> => {
+    const logger = new Logger("WinesApp");
+    if (!json) {
+        return new Map();
     }
-
-    private serializeFilters() {
-        if (this.state.filterTexts) {
-            const predStrings = Array.from(this.state.filterTexts.entries());
-            this.logger.logInfo(`Filter texts: '${predStrings}'`)
-            const serializedPredicates = JSON.stringify(predStrings);
-            this.logger.logDebug(`Updating cookie to '${serializedPredicates}'`);
-            window.localStorage.setItem(WinesApp.localStorageKey, serializedPredicates);
-        } else {
-            window.localStorage.removeItem(WinesApp.localStorageKey);
-        }
-    }
-
-    private deserializeFilters(json: string): Map<WinesTableColumn, string> {
-        if (!json) {
-            return new Map();
-        }
-        this.logger.logDebug(`Deserializing JSON: ${json}`);
-        const predicates = new Map();
-        try {
-            const arr: Array<[string, string]> = JSON.parse(json);
-            arr.forEach((item) => {
-                const [key, text] = item;
-                predicates.set(key, text);
-            });
-            return predicates;
-        } catch (err) {
-            window.localStorage.removeItem(WinesApp.localStorageKey);
-            this.logger.logWarning(`Failed reading filters cookie with error: ${err}`);
-            return new Map();
-        }
-    }
-
-    private parseAllFilters(filterTexts: Map<WinesTableColumn, string>): Map<WinesTableColumn, FilterExpr> {
-        const predicates = new Map<WinesTableColumn, FilterExpr>()
-        for (const entry of filterTexts.entries()) {
-            predicates.set(entry[0], FilterExpr.parse(entry[1]));
-        }
+    logger.logDebug(`Deserializing JSON: ${json}`);
+    const predicates = new Map();
+    try {
+        const arr: Array<[string, string]> = JSON.parse(json);
+        arr.forEach((item) => {
+            const [key, text] = item;
+            predicates.set(key, text);
+        });
         return predicates;
+    } catch (err) {
+        window.localStorage.removeItem(LOCAL_STORAGE_KEY);
+        logger.logWarning(`Failed reading filters cookie with error: ${err}`);
+        return new Map();
     }
+}
 
-    public render() {
-        if (!this.state.hasLoaded) {
-            return <Preloader />;
+const parseAllFilters = (filterTexts: Map<WinesTableColumn, string>): Map<WinesTableColumn, FilterExpr> => {
+    const predicates = new Map<WinesTableColumn, FilterExpr>()
+    for (const entry of filterTexts.entries()) {
+        predicates.set(entry[0], FilterExpr.parse(entry[1]));
+    }
+    return predicates;
+}
+
+const initState = (): IState => {
+    const filterTexts = deserializeFilters(window.localStorage.getItem(LOCAL_STORAGE_KEY) ?? "");
+    return {
+        wines: [],
+        filterTexts,
+        predicates: parseAllFilters(filterTexts),
+        hasLoaded: false,
+        currentPage: 1,
+        winesPerPage: 50,
+    }
+};
+
+const reducer: React.Reducer<IState, Action> = (state, action) => {
+    switch (action.type) {
+        case "setWines":
+            return { ...state, wines: action.wines, hasLoaded: true };
+        case "setFilter":
+            const predicates = new Map(state.predicates);
+            const filterTexts = new Map(state.filterTexts);
+            predicates.set(action.column, FilterExpr.parse(action.text));
+            filterTexts.set(action.column, action.text);
+            return { ...state, predicates, filterTexts };
+        case "resetFilters":
+            return { ...state, predicates: new Map(), filterTexts: new Map() };
+        case "setCurrentPage":
+            return { ...state, currentPage: action.currentPage };
+        default:
+            return state;
+    }
+}
+
+export const WinesApp: React.FC<{}> = (_) => {
+    const logger = new Logger("WinesApp");
+    useTitle("Wines");
+
+    const [state, dispatch] = React.useReducer(reducer, initState())
+
+    React.useEffect(() => {
+        async function fetchWines() {
+            try {
+                const wines = await getWines({});
+                dispatch({type: "setWines", wines});
+            } catch (e) {
+                logger.logError(`Failed to get wines: ${e.message}`);
+                dispatch({type: "setWines", wines: []});
+            }
         }
-        let wines = undefined;
-        if (this.state.wines.length === 0) {
-            wines = (
-                <div className="center-align">
-                    <h6 className="bold">
-                        You haven&rsquo;t entered any wines yet.
-                    </h6>
-                    <BtnLink classes={ ["green-bg"] }
-                        to="/wines/new/"
-                    >
-                        Add a new wine
-                    </BtnLink>
-                </div>
-            );
+
+        fetchWines();
+    }, []);
+
+    // Update local storage predicates when they change
+    React.useEffect(() => {
+        if (state.filterTexts) {
+            const predStrings = Array.from(state.filterTexts.entries());
+            logger.logInfo(`Filter texts: '${predStrings}'`)
+            const serializedPredicates = JSON.stringify(predStrings);
+            logger.logDebug(`Updating cookie to '${serializedPredicates}'`);
+            window.localStorage.setItem(LOCAL_STORAGE_KEY, serializedPredicates);
         } else {
-            wines = (
-                <>
-                    <h3 className="page-title">Wines</h3>
-                    <Btn classes={ ["yellow-bg"] }
-                        onClick={ this.onResetFilters.bind(this) }
-                    >
-                        Reset Filters
-                    </Btn>
-                    <WinesTable onFilterChange={ (c, text) => this.onFilterChange(c, text) }
-                        wines={ this.filteredWines }
-                        filterTexts={ this.state.filterTexts }
-                        currentPage={ this.state.currentPage }
-                        winesPerPage={ this.state.winesPerPage }
-                    />
-                    <Pagination currentPage={ this.state.currentPage }
-                        pageCount={ Math.max(1,
-                            Math.ceil(this.filteredWines.length / this.state.winesPerPage )) }
-                        onClick={ this.onPageClick.bind(this) }
-                    />
-                </>
-            );
+            window.localStorage.removeItem(LOCAL_STORAGE_KEY);
         }
-        return (
-            <div className="container">
-                <Row>
-                    <Col s={ 12 }>
-                        { wines }
-                    </Col>
-                </Row>
+    }, [state.filterTexts]);
+
+    // Rendering
+    if (!state.hasLoaded) {
+        return <Preloader />;
+    }
+    let winesComponent = undefined;
+    if (state.wines.length === 0) {
+        winesComponent = (
+            <div className="center-align">
+                <h6 className="bold">
+                    You haven&rsquo;t entered any wines yet.
+                </h6>
+                <BtnLink classes={ ["green-bg"] }
+                    to="/wines/new/"
+                >
+                    Add a new wine
+                </BtnLink>
             </div>
         );
-    }
-
-    public async componentDidMount() {
-        try {
-            setTitle("Wines");
-            const wines = await getWines({});
-            this.setState({
-                wines,
-                hasLoaded: true
-            });
-        } catch (e) {
-            this.logger.logError(`Failed to get wines: ${e.message}`);
-        }
-    }
-
-    private get filteredWines() {
+    } else {
         // Reduce predicates
-        const combinedPred = [...this.state.predicates.entries()]
+        const combinedPred = [...state.predicates.entries()]
             .reduceRight((prevVal, [column, filterExpr]) => {
                 return (wine) => prevVal(wine) && filterExpr.call(columnToVal(column, wine)!);
             }, (_: IWine) => true);
-        return this.state.wines.filter(combinedPred);
-    }
+        const filteredWines = state.wines.filter(combinedPred);
 
-    private onFilterChange(column: WinesTableColumn, text: string) {
-        this.setState((prevState) => {
-            prevState.predicates.set(column, FilterExpr.parse(text));
-            prevState.filterTexts.set(column, text);
-            return {
-                predicates: prevState.predicates,
-                filterTexts: prevState.filterTexts,
-            };
-        }, this.serializeFilters);
+        winesComponent = (
+            <>
+                <h3 className="page-title">Wines</h3>
+                <Btn classes={ ["yellow-bg"] }
+                    onClick={ () => dispatch({type: "resetFilters"}) }
+                >
+                    Reset Filters
+                </Btn>
+                <WinesTable onFilterChange={ (column, text) => dispatch({type: "setFilter", column, text}) }
+                    wines={ filteredWines }
+                    filterTexts={ state.filterTexts }
+                    currentPage={ state.currentPage }
+                    winesPerPage={ state.winesPerPage }
+                />
+                <Pagination currentPage={ state.currentPage }
+                    pageCount={ Math.max(1,
+                        Math.ceil(filteredWines.length / state.winesPerPage )) }
+                    onClick={ (currentPage) => dispatch({type: "setCurrentPage", currentPage}) }
+                />
+            </>
+        );
     }
-
-    private onResetFilters() {
-        this.setState({
-            predicates: new Map(),
-            filterTexts: new Map(),
-        }, this.serializeFilters);
-    }
-
-    private onPageClick(pageNumber: number) {
-        if (pageNumber === this.state.currentPage) {
-            return;
-        }
-        this.setState({
-            currentPage: pageNumber,
-        });
-    }
+    return (
+        <div className="container">
+            <Row>
+                <Col s={ 12 }>
+                    { winesComponent }
+                </Col>
+            </Row>
+        </div>
+    );
 }
+WinesApp.displayName = "WinesApp";
