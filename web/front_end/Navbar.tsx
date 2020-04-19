34bccac2fc03d71257d7c49419423824e010aab2
Carter Green <cartergr@usc.edu>
2020-04-19 18:37:37 -0500

    Fix hooks ordering

--- web/front_end/Navbar.tsx
+++ web/front_end/Navbar.tsx
@@ -91,89 +91,105 @@ const NavLink: React.FC<{to: string}> = ({to, ...props}) => {
     );
 }
 
-const MenuItems: React.FC<{id: string} & INavProps> = ({id, setModalState}) => {
+interface IMenuItemsProps extends INavProps {
+    id: string
+}
+
+const MenuItems: React.FC<IMenuItemsProps> = (props) => {
     const user = useUser();
-    const setUser = useSetUser();
 
     if (user) {
-        const addDropdownRef = React.useRef() as React.MutableRefObject<HTMLAnchorElement>;
-        const userDropdownRef = React.useRef() as React.MutableRefObject<HTMLAnchorElement>;
-        React.useEffect(() => {
-            // tslint:disable-next-line no-unused-expression
-            new Dropdown(addDropdownRef.current);
-        }, [addDropdownRef]);
-        React.useEffect(() => {
-            // tslint:disable-next-line no-unused-expression
-            new Dropdown(userDropdownRef.current);
-        }, [userDropdownRef]);
-
-        const onLogout = async (e: React.MouseEvent) => {
-            e.preventDefault();
-            await logout();
-            setUser(null);
-            redirectTo("/");
-        }
-
-        return (
-            <>
-                <li>
-                    <ul id={ `${id}-add` } className="dropdown-content">
-                        <NavLink to="/wines/new">
-                            New Wine
-                        </NavLink>
-                        <NavLink to="/wines/search">
-                            Purchased Again
-                        </NavLink>
-                    </ul>
-                    <a href="#!" className="dropdown-trigger" data-target={ `${id}-add` }
-                        ref={ addDropdownRef }
-                    >
-                        <MaterialIcon className="left" iconName="add_circle" />
-                        Add
-                        <MaterialIcon className="right" iconName="arrow_drop_down" />
-                    </a>
-                </li>
-                <NavLink to="/wines">
-                    <MaterialIcon className="left" iconName="reorder" />
-                    Wines
-                </NavLink>
-                <NavLink to="/dashboards">
-                    <MaterialIcon className="left" iconName="dashboard" />
-                    Dashboards
-                </NavLink>
-                <NavLink to="/wines/inventory">
-                    <MaterialIcon className="left" iconName="view_comfy" />
-                    Inventory
-                </NavLink>
-                <li>
-                    <ul id={ `${id}-user` } className="dropdown-content">
-                        <NavLink to="/profile">
-                            Profile
-                        </NavLink>
-                        <li className="sidenav-close">
-                            <a href="#!" onClick={ onLogout }>
-                                Log out
-                            </a>
-                        </li>
-                    </ul>
-                    <a href="#!" className="dropdown-trigger" data-target={ `${id}-user` }
-                        ref={ userDropdownRef }
-                    >
-                        <MaterialIcon className="left" iconName="account_circle" />
-                        { user.name }
-                        <MaterialIcon className="right" iconName="arrow_drop_down" />
-                    </a>
-                </li>
-            </>
-        );
+        <UserMenuItems {...props} />
     }
     return (
-        <li id="login-nav" className={ ModalState.Login ? "active" : "" } >
-            <a onClick={ () => setModalState(ModalState.Login) }>
-                <MaterialIcon className="left" iconName="account_circle" />
-                Login
-            </a>
-        </li>
+        <NoUserMenuItems {...props} />
     );
 }
 MenuItems.displayName = "MenuItems";
+
+const UserMenuItems: React.FC<IMenuItemsProps> = ({id}) => {
+    const user = useUser();
+    const setUser = useSetUser();
+
+    const addDropdownRef = React.useRef() as React.MutableRefObject<HTMLAnchorElement>;
+    const userDropdownRef = React.useRef() as React.MutableRefObject<HTMLAnchorElement>;
+    React.useEffect(() => {
+        // tslint:disable-next-line no-unused-expression
+        new Dropdown(addDropdownRef.current);
+    }, [addDropdownRef]);
+    React.useEffect(() => {
+        // tslint:disable-next-line no-unused-expression
+        new Dropdown(userDropdownRef.current);
+    }, [userDropdownRef]);
+
+    const onLogout = async (e: React.MouseEvent) => {
+        e.preventDefault();
+        await logout();
+        setUser(null);
+        redirectTo("/");
+    }
+
+    return (
+        <>
+            <li>
+                <ul id={ `${id}-add` } className="dropdown-content">
+                    <NavLink to="/wines/new">
+                        New Wine
+                    </NavLink>
+                    <NavLink to="/wines/search">
+                        Purchased Again
+                    </NavLink>
+                </ul>
+                <a href="#!" className="dropdown-trigger" data-target={ `${id}-add` }
+                    ref={ addDropdownRef }
+                >
+                    <MaterialIcon className="left" iconName="add_circle" />
+                    Add
+                    <MaterialIcon className="right" iconName="arrow_drop_down" />
+                </a>
+            </li>
+            <NavLink to="/wines">
+                <MaterialIcon className="left" iconName="reorder" />
+                Wines
+            </NavLink>
+            <NavLink to="/dashboards">
+                <MaterialIcon className="left" iconName="dashboard" />
+                Dashboards
+            </NavLink>
+            <NavLink to="/wines/inventory">
+                <MaterialIcon className="left" iconName="view_comfy" />
+                Inventory
+            </NavLink>
+            <li>
+                <ul id={ `${id}-user` } className="dropdown-content">
+                    <NavLink to="/profile">
+                        Profile
+                    </NavLink>
+                    <li className="sidenav-close">
+                        <a href="#!" onClick={ onLogout }>
+                            Log out
+                        </a>
+                    </li>
+                </ul>
+                <a href="#!" className="dropdown-trigger" data-target={ `${id}-user` }
+                    ref={ userDropdownRef }
+                >
+                    <MaterialIcon className="left" iconName="account_circle" />
+                    { user?.name ?? "" }
+                    <MaterialIcon className="right" iconName="arrow_drop_down" />
+                </a>
+            </li>
+        </>
+    );
+}
+UserMenuItems.displayName = "UserMenuItems";
+
+const NoUserMenuItems: React.FC<IMenuItemsProps> = ({setModalState}) => (
+    <li id="login-nav" className={ ModalState.Login ? "active" : "" } >
+        <a onClick={ () => setModalState(ModalState.Login) }>
+            <MaterialIcon className="left" iconName="account_circle" />
+            Login
+        </a>
+    </li>
+);
+NoUserMenuItems.displayName = "NoUserMenuItems";
