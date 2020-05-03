4d4d9db5f4548876f6efcf95f4403ebe307b626f
Carter Green <cartergr@usc.edu>
2020-05-03 11:23:49 -0500

    Fix utf-8 in csv

--- web/lib/csv.ts
+++ web/lib/csv.ts
@@ -15,8 +15,9 @@ export function generateCSV<O>(objects: O[], columnOrder: Array<keyof O>,
     }, columnOrder.join(","));
 }
 
-export function download(fileName: string, data: string, type = "text/csv") {
-    const blob = new Blob([data], {type});
+export function download(fileName: string, data: string, type = "text/csv;charset=UTF-8") {
+    // BOM for utf-8 encoding
+    const blob = new Blob(["\ufeff" + data], {type});
     // Create a link element, click it, then destroy it
     const elem = window.document.createElement("a");
     elem.href = window.URL.createObjectURL(blob);
