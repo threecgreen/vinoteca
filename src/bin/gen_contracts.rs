ac3d9ba45688556b279fa9ec12cfd90fbee7b2d4
Carter Green <cartergr@usc.edu>
2020-05-31 00:15:17 -0500

    Fix newline

--- src/bin/gen_contracts.rs
+++ src/bin/gen_contracts.rs
@@ -50,7 +50,7 @@ fn write_log_level(writer: &mut BufWriter<&File>) -> io::Result<()> {
 fn write_sha(writer: &mut BufWriter<&File>) -> Result<(), Box<dyn error::Error>> {
     let output = std::process::Command::new("git").args(&["rev-parse", "HEAD"]).output()?;
     let sha = String::from_utf8(output.stdout)?;
-    Ok(write_const(writer, "GIT_SHA", &sha)?)
+    Ok(write_const(writer, "GIT_SHA", sha.trim())?)
 }
 
 fn write_contracts() ->  Result<(), Box<dyn error::Error>> {
