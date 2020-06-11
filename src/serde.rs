8003e5769984f692ae97256a215b0229b7d25aa6
Carter Green <cartergr@usc.edu>
2020-06-11 11:51:24 -0500

    Add serde tests

--- src/serde.rs
+++ src/serde.rs
@@ -22,5 +22,67 @@ where
     D: Deserializer<'de>,
 {
     let s: Option<String> = Deserialize::deserialize(deserializer)?;
-    Ok(s.map(|s| s.trim().to_owned()))
+    Ok(s.and_then(|s| {
+        let trimmed_s = s.trim().to_owned();
+        if trimmed_s.is_empty() {
+            None
+        } else {
+            Some(trimmed_s)
+        }
+    }))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use serde::de::IntoDeserializer;
+    use serde::de::value::{BorrowedStrDeserializer, StringDeserializer, Error};
+
+    #[test]
+    fn trim_str_untouched() {
+        let test_str = "quick brown fox";
+        let deserializer = BorrowedStrDeserializer::<Error>::new(test_str);
+        assert_eq!(trim_str(deserializer), Ok(test_str));
+    }
+
+    #[test]
+    fn trim_str_all_spaces() {
+        let spaces = " ";
+        let deserializer = BorrowedStrDeserializer::<Error>::new(spaces);
+        assert_eq!(trim_str(deserializer), Ok(""));
+    }
+
+    #[test]
+    fn trim_str_leading_and_trailing() {
+        let deserializer = BorrowedStrDeserializer::<Error>::new("  the Quick brown fox  ");
+        assert_eq!(trim_str(deserializer), Ok("the Quick brown fox"));
+    }
+
+    #[test]
+    fn trim_string_untouched() {
+        let test_string = "quick brown fox".to_owned();
+        let deserializer: StringDeserializer<Error> = test_string.clone().into_deserializer();
+        assert_eq!(trim_string(deserializer), Ok(test_string));
+    }
+
+    #[test]
+    fn trim_string_all_spaces() {
+        let deserializer: StringDeserializer<Error> = "    ".to_owned().into_deserializer();
+        assert_eq!(trim_string(deserializer), Ok("".to_owned()));
+    }
+
+    #[test]
+    fn trim_string_leading_and_trailing() {
+        let deserializer: StringDeserializer<Error> = "  the Quick brown fox  ".to_owned().into_deserializer();
+        assert_eq!(trim_string(deserializer), Ok("the Quick brown fox".to_owned()));
+    }
+
+    #[test]
+    #[ignore]
+    fn trim_opt_only_spaces_to_none() {
+        // let deserializer = serde_json::de::Deserializer::from_str(r#""   ""#);
+        // let deserializer = Some("   ".to_owned()).into_deserializer();
+        // assert_eq!(trim_opt_string(deserializer), Ok(None));
+    }
 }
