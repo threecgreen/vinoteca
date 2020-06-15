b3630dd2e19fa133f403a58a18dd5634710caaa3
Carter Green <cartergr@usc.edu>
2020-06-14 21:10:54 -0500

    Remove unneeded logs in catchers

--- src/catchers.rs
+++ src/catchers.rs
@@ -2,12 +2,10 @@ use crate::error::VinotecaError;
 
 #[catch(401)]
 pub fn unauthorized() -> VinotecaError {
-    warn!("Handling unauthorized request");
     VinotecaError::Unauthorized("Login required".to_owned())
 }
 
 #[catch(403)]
 pub fn forbidden() -> VinotecaError {
-    warn!("Handling forbidden request");
     VinotecaError::Forbidden("Bad email or password".to_owned())
 }
