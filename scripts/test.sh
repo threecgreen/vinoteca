a48e399be4fe3cf055827bf46e3e27b78f613f89
Carter Green <cartergr@usc.edu>
2020-06-23 04:31:00 -0500

    Decrease rust test verbosity

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,7 @@
 # Run unit tests
 source "$(dirname $0)/utils.sh"
 
-cd "$root_dir"
-if [ "$CI" == "true" ]; then
-    cargo test --verbose || error_exit "Rust test(s) failed"
-else
-    cargo test || error_exit "Rust test(s) failed"
-fi
-cd -
+cargo test || error_exit "Rust test(s) failed"
 cd "$root_dir/web"
 npm run test || error_exit "JS test(s) failed"
 cd -
