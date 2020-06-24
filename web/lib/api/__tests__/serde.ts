ef31f9a1b343811b888453d5c5870b49dcfb1e40
Carter Green <cartergr@usc.edu>
2020-06-24 10:06:15 -0500

    Enhanced JSON utils

---
+++ web/lib/api/__tests__/serde.ts
@@ -0,0 +1,33 @@
+import { Json } from "../serde";
+
+test("Deserializing nullable Date leaves null alone", () => {
+    const obj = {date: null, fruit: "Apple", count: 1};
+    const input = JSON.stringify(obj);
+
+    const result = Json.parse(input, {dateKeys: ["date"]}) as typeof obj;
+    expect(result.date).toBeNull();
+});
+
+test("Serializing nullable Date leaves null alone", () => {
+    const obj = {date: null, fruit: "Pear", count: 2.5};
+    const result = Json.stringify(obj);
+    expect(result).toContain("null");
+});
+
+test("(De)serialize date", () => {
+    const obj = {date: new Date(2006, 12, 25), fruit: "peach", count: -3};
+
+    console.log(obj);
+    const objString = Json.stringify(obj, {dateKeys: ["date"]});
+    const result = Json.parse(objString, {dateKeys: ["date"]}) as typeof obj;
+    expect(result).toStrictEqual(obj);
+});
+
+
+test("Deserialize datetime", () => {
+    const obj = {dateTime: new Date(2018, 6, 26, 13, 30), count: 0.75};
+    const input = JSON.stringify(obj);
+
+    const result = Json.parse(input, {dateTimeKeys: ["dateTime"]}) as typeof obj;
+    expect(result).toStrictEqual(obj);
+});
---
+++ web/lib/api/serde.ts
@@ -0,0 +1,77 @@
+import { deserializeDate, serializeDate } from "../date";
+
+interface IParseParams<T> {
+    dateKeys: Array<keyof T>;
+    dateTimeKeys: Array<keyof T>;
+}
+
+interface IStringifyParams<T> {
+    dateKeys: Array<keyof T>;
+}
+
+/**
+ * JSON class with improved de(ser)erialization of dates and datetimes.
+ */
+export class Json {
+    public static parse<T>(
+        text: string, { dateKeys, dateTimeKeys }: Partial<IParseParams<T>> = {},
+    ): T | T[] {
+        if (dateKeys || dateTimeKeys) {
+            return JSON.parse(
+                text,
+                Json.createReviver(
+                    dateKeys ?? [],
+                    dateTimeKeys ?? [],
+                ) as (this: any, key: string, value: any) => any,
+            );
+        }
+        return JSON.parse(text);
+    }
+
+    public static stringify<T>(
+        value: T | T[], { dateKeys }: Partial<IStringifyParams<T>> = {},
+    ): string {
+        if (dateKeys) {
+            return JSON.stringify(
+                value,
+                Json.createReplacer(
+                    dateKeys,
+                ) as (this: any, key: string, value: any) => string,
+            );
+        }
+        return JSON.stringify(value);
+    }
+
+    private static createReviver<T>(
+        dateKeys: Array<keyof T>,
+        dateTimeKeys: Array<keyof T>,
+    ): (this: any, key: keyof T, value: any) => any {
+        return (key, value) =>  {
+            if (value === null) {
+                return value;
+            }
+            if (dateKeys.includes(key)) {
+                return deserializeDate(value);
+            }
+            if (dateTimeKeys.includes(key)) {
+                return new Date(value);
+            }
+            return value;
+        };
+    }
+
+    private static createReplacer<T>(
+        dateKeys: Array<keyof T>,
+    ): (this: any, key: keyof T, value: any) => string {
+        return (key, value) => {
+            if (value === null) {
+                return value;
+            }
+            if (dateKeys.includes(key)) {
+                // Yes we have to deserialize to re-serialize
+                return serializeDate(new Date(value));
+            }
+            return value;
+        };
+    }
+}
