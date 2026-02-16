7ab4d97c45cf53043d8ca7f232676eb51bbbc67a
Carter Green <crtrgreen@gmail.com>
2026-02-16 16:30:47 -0600

    Improve font

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   ],
   theme: {
     fontFamily: {
-      sans: ['Roboto', 'sans-serif'],
+      sans: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', '"Helvetica Neue"', 'sans-serif'],
     },
     extend: {
       colors: {
